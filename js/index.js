446afe92349fdfaf2f093489d0f8540ce8721dcc
Nata-ta <59442094+Nata-ta@users.noreply.github.com>
2022-09-09 22:05:08 +0300

    added weather-app project

---
+++ js/index.js
@@ -0,0 +1,111 @@
+const getElement = (id) => document.getElementById(id);
+
+const city = getElement('current-city');
+const humidity = getElement('current-humidity');
+const pressure = getElement('current-pressure');
+const temperature = getElement('current-temperature');
+const windSpeed = getElement('current-wind-speed');
+const summary = getElement('current-weather-summary');
+
+const getWeatherButton = getElement('getCurrentWeather');
+
+const requiredCity = getElement('required-city');
+const getRequiredWeather = getElement('getRequiredWeather');
+
+const error = getElement('error-info');
+const closeError = getElement('error-close');
+
+const loading = getElement('loading');
+
+const weatherAPI = 'f99657535e4042513e47effacfb921b2';
+
+const mathRound = (temp) => Math.round(temp);
+
+const celciumTemprerature = (data) => data.main.temp - 273.15;
+
+const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        getGeoData(position.coords.latitude, position.coords.longitude);
+    });
+};
+
+const getGeoData = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherAPI}`)
+        .then(response => response.json())
+        .then(data => displayGeoData(data))
+};
+
+const displayGeoData = (data) => {
+    console.log(data, 'data');
+
+    if (!data) {
+        loading.style.display = "block";
+    } else {
+        requiredCity.value = "";
+        city.innerHTML = "";
+        humidity.innerHTML = "";
+        pressure.innerHTML = "";
+        temperature.innerHTML = "";
+        windSpeed.innerHTML = "";
+        summary.innerHTML = "";
+
+        city.innerHTML = `${data.name}, ${data.sys.country}`;
+        temperature.innerHTML = `${mathRound(celciumTemprerature(data))} &#8451;`;
+
+        const weatherIcon = document.createElement('img');
+        weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+        const weatherDescr = document.createElement('span');
+        weatherDescr.innerHTML = `${data.weather[0].main}`;
+
+        const humidityIcon = document.createElement('i');
+        humidityIcon.classList.add("fa", "fa-solid", "fa-water");
+
+        const humidityDescr = document.createElement('p');
+        humidityDescr.innerHTML = `Humidity: ${data.main.humidity}&#65285;`;
+        humidityDescr.classList.add("info__wrap-descr");
+
+        const pressureIcon = document.createElement('i');
+        pressureIcon.classList.add("fa", "fa-brands", "fa-safari");
+
+        const pressureDescr = document.createElement('p');
+        pressureDescr.innerHTML = `Pressure: ${data.main.pressure}`;
+        pressureDescr.classList.add("info__wrap-descr");
+
+        const windSpeedIcon = document.createElement('i');
+        windSpeedIcon.classList.add("fa", "fa-solid", "fa-wind");
+
+        const windSpeedDescr = document.createElement('p');
+        windSpeedDescr.innerHTML = `Wind Speed: ${data.wind.speed}mps`;
+        windSpeedDescr.classList.add("info__wrap-descr");
+
+        summary.appendChild(weatherIcon);
+        summary.appendChild(weatherDescr);
+
+        humidityDescr.appendChild(humidityIcon);
+        humidity.appendChild(humidityDescr);
+
+        pressureDescr.appendChild(pressureIcon);
+        pressure.appendChild(pressureDescr);
+
+        windSpeedDescr.appendChild(windSpeedIcon);
+        windSpeed.appendChild(windSpeedDescr);
+    }
+};
+
+const getLocation = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${requiredCity.value}&appid=${weatherAPI}`)
+        .then(response => response.json())
+        .then(data => displayGeoData(data))
+        .catch(err => {
+            error.style.display = "block";
+        })
+};
+
+closeError.addEventListener('click', function () {
+    error.style.display = "none";
+})
+
+getWeatherButton.addEventListener('click', getCurrentLocation);
+
+getRequiredWeather.addEventListener('click', getLocation);
